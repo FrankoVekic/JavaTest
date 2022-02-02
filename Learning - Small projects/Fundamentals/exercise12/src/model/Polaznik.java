4d53416f5fd03cccc08135d65bd94ed76253174f
Franko Vekić <franko.vekic@gmail.com>
2022-02-02 23:04:00 +0100

    Starting to create a basic crud app

---
+++ Learning - Small projects/Fundamentals/exercise12/src/model/Polaznik.java
@@ -0,0 +1,17 @@
+package model;
+
+public class Polaznik extends Osoba {
+
+	private String brojugovora;
+
+	public String getBrojugovora() {
+		return brojugovora;
+	}
+
+	public void setBrojugovora(String brojugovora) {
+		this.brojugovora = brojugovora;
+	}
+	
+	
+	
+}
---
+++ Learning - Small projects/Fundamentals/exercise12/src/module-info.java
@@ -0,0 +1,4 @@
+module exercise12 {
+	requires java.desktop;
+	requires java.sql;
+}
