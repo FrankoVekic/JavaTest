ea01bbf74f809ec4f596d01e7d07483d8f482d67
Franko Vekić <franko.vekic@gmail.com>
2022-03-08 11:12:36 +0100

    Menu created

---
+++ Learning - Small projects/NetBeans/exerciseFromHome/src/main/java/view/Menu.java
@@ -0,0 +1,136 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+ */
+package view;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import util.Util;
+
+/**
+ *
+ * @author frank
+ */
+public class Menu extends javax.swing.JFrame {
+
+    private SimpleDateFormat df;
+
+    public Menu() {
+        initComponents();
+        setTitle(Util.getTitle("Menu"));
+        jmApp.setText(Util.APP_TITLE);
+        df = new SimpleDateFormat("dd. MMMM. yyy. HH:mm:ss");
+        df = new SimpleDateFormat("HH:mm:ss");
+        Time t = new Time();
+        t.start();
+        
+    }
+    
+    private class Time extends Thread{
+        
+        @Override
+        public void run(){
+            lblTime.setText(df.format(new Date()));
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                
+            }
+            run();
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jToolBar1 = new javax.swing.JToolBar();
+        lblTime = new javax.swing.JLabel();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jmApp = new javax.swing.JMenu();
+        jMenuItem1 = new javax.swing.JMenuItem();
+        jMenu2 = new javax.swing.JMenu();
+        jMenuItem2 = new javax.swing.JMenuItem();
+        jMenuItem3 = new javax.swing.JMenuItem();
+        jMenuItem4 = new javax.swing.JMenuItem();
+        jMenuItem5 = new javax.swing.JMenuItem();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jToolBar1.setRollover(true);
+
+        lblTime.setText("jLabel1");
+        jToolBar1.add(lblTime);
+
+        jmApp.setText("File");
+
+        jMenuItem1.setText("Exit");
+        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem1ActionPerformed(evt);
+            }
+        });
+        jmApp.add(jMenuItem1);
+
+        jMenuBar1.add(jmApp);
+
+        jMenu2.setText("Programs");
+
+        jMenuItem2.setText("Courses");
+        jMenu2.add(jMenuItem2);
+
+        jMenuItem3.setText("Professors");
+        jMenu2.add(jMenuItem3);
+
+        jMenuItem4.setText("Students");
+        jMenu2.add(jMenuItem4);
+
+        jMenuItem5.setText("Groups");
+        jMenu2.add(jMenuItem5);
+
+        jMenuBar1.add(jMenu2);
+
+        setJMenuBar(jMenuBar1);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jToolBar1, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 300, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(0, 255, Short.MAX_VALUE)
+                .addComponent(jToolBar1, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+       dispose();
+    }//GEN-LAST:event_jMenuItem1ActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+   
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JMenuItem jMenuItem3;
+    private javax.swing.JMenuItem jMenuItem4;
+    private javax.swing.JMenuItem jMenuItem5;
+    private javax.swing.JToolBar jToolBar1;
+    private javax.swing.JMenu jmApp;
+    private javax.swing.JLabel lblTime;
+    // End of variables declaration//GEN-END:variables
+}
